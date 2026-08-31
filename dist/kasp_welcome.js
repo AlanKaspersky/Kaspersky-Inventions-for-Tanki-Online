1c97c3f6071137be9ebb707f8f8c1bdb064781a4
Alan Kaspersky <mvzlom997@gmail.com>
2026-08-31 06:03:57 +0300

    Release 1.6

--- dist/kasp_welcome.js
+++ dist/kasp_welcome.js
@@ -4,7 +4,7 @@
         return;
     }
     'use strict';
-    const CURRENT_VERSION = '1.5.1';
+    const CURRENT_VERSION = '1.6';
     const STORAGE_KEY = 'kasp_last_version';
     function getLang() {
         const htmlLang = document.documentElement.lang || '';
